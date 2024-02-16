3f9c2a0cc65adb9d0e527e0b6e80b5ce142b7386
Beboye <80493240+Beboye@users.noreply.github.com>
2024-02-16 18:11:30 +0800

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Select the script element by its tag name
-    var scriptElement = document.querySelector('script');
-
-    // Check if the script element is found
-    if (scriptElement) {
-        // Update the src attribute
-        scriptElement.src = scriptElement.src.replace('https://dev-pitgurat.pantheonsite.io/sandbox/', 'https://mycdn.com/');
-    }
-});
+function changeScriptSrc(newBaseUrl) {
+    var script = document.getElementById('myScript');
+    var currentSrc = script.src;
+    var pathAfterBaseUrl = currentSrc.replace(/^.*\/\/[^\/]+/, '');
+    var newSrc = newBaseUrl + pathAfterBaseUrl;
+    script.src = newSrc;
+}
+changeScriptSrc('https://mycdn.com/');
